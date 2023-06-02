3fa3e821ab36d82afd5ef45d77cdbbb1b7c1330d
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-06-02 20:31:11 +0200

    Add pundit to application (#10)

---
+++ app/policies/application_policy.rb
@@ -0,0 +1,34 @@
+class ApplicationPolicy
+  attr_reader :user, :record
+
+  def initialize(user, record)
+    @user = user
+    @record = record
+  end
+
+  def index?
+    false
+  end
+
+  alias_method :show?, :index?
+  alias_method :new?, :create?
+  alias_method :create?, :index?
+  alias_method :edit?, :update?
+  alias_method :update?, :index?
+  alias_method :destroy?, :index?
+
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      raise NotImplementedError, "You must define #resolve in #{self.class}"
+    end
+
+    private
+
+    attr_reader :user, :scope
+  end
+end
