93818adbd211adaf78a2de1a7be7c97e3144af4e
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-06-12 21:14:42 +0200

    Add application policy spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'pundit/rspec'
+
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   config.add_formatter 'Fuubar'
