86b4b686b304d2ad339b8986bb3b75ca7903c196
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-06-23 22:14:16 +0200

    Simplify view component helper (#50)

--- app/helpers/view_components_helper.rb
+++ app/helpers/view_components_helper.rb
@@ -2,18 +2,10 @@
 
 module ViewComponentsHelper
   def render_component(klass, *args, **kwargs, &)
-    if public_methods.include? :view_context
-      klass.new(*args, **kwargs).render_in(view_context)
-    elsif public_methods.include? :render
-      render(klass.new(*args, **kwargs), &)
-    end
+    render(klass.new(*args, **kwargs), &)
   end
 
   def render_component_collection(klass, *args, **kwargs, &)
-    if public_methods.include? :view_context
-      klass.with_collection(*args, **kwargs).render_in(view_context)
-    elsif public_methods.include? :render
-      render(klass.with_collection(*args, **kwargs, &))
-    end
+    render(klass.with_collection(*args, **kwargs, &))
   end
 end
