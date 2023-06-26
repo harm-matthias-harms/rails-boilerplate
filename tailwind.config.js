b410ac3a79c548c25942ea00e8aa69ed07873c51
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-06-26 22:36:22 +0200

    Improve tailwind content for components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './app/views/**/*.html.haml',
-    './app/components/**/*.html.haml',
+    './app/components/**/*.{html.haml,rb,js}',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
