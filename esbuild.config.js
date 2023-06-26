06d66df1b209fe4e3e3b05776d83135954509ed8
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-06-26 22:39:02 +0200

    Add local nunito font (#56)
    * Add local nunito font
    
    * Add esbuild.config.js
    
    * Cleanup esbuild config
    
    * Rename baseConfig to globalOptions
    
    * Improve tailwind content for components
    
    * Remove bin node

---
+++ esbuild.config.js
@@ -0,0 +1,40 @@
+import * as esbuild from 'esbuild'
+import path from 'path'
+
+async function buildContext (options) {
+  const ctx = await esbuild.context(options)
+
+  if (process.argv.includes('--watch')) {
+    await ctx.watch()
+  } else {
+    await ctx.rebuild()
+    await ctx.dispose()
+  }
+}
+
+const globalOptions = {
+  sourcemap: true,
+  bundle: true,
+  minify: true,
+  logLevel: 'debug'
+}
+
+buildContext({
+  ...globalOptions,
+  entryPoints: ['application.js'],
+  outdir: path.join(process.cwd(), 'app/assets/builds'),
+  absWorkingDir: path.join(process.cwd(), 'app/javascript')
+})
+
+buildContext({
+  ...globalOptions,
+  entryPoints: ['nunito.css'],
+  outdir: path.join(process.cwd(), 'app/assets/builds/fonts'),
+  absWorkingDir: path.join(process.cwd(), 'app/assets/fonts'),
+  assetNames: '[name]-00000000.digested',
+  entryNames: '[name]-00000000.digested',
+  loader: {
+    '.woff': 'file',
+    '.woff2': 'file'
+  }
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.html.haml',
+    './app/components/**/*.{html.haml,rb,js}',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
   ],
   plugins: [require('daisyui')],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
   daisyui: {
     themes: ['winter']
   }
