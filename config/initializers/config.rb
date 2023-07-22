8bf0065ffa89dee3aed581089b930a05ddc0e086
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-07-22 09:32:49 +0200

    S3 should always be defined with optional values

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -75,6 +75,11 @@ Config.setup do |config|
         end
       end
       required(:s3).schema do
+        required(:endpoint).maybe(:string)
+        required(:region).maybe(:string)
+        required(:bucket).maybe(:string)
+        required(:access_key_id).maybe(:string)
+        required(:secret_access_key).maybe(:string)
         if Rails.env.production?
           required(:endpoint).filled(:string)
           required(:region).filled(:string)
