897655473a78d59cfc6a295a44115f8ddac70c5c
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-07-26 22:10:29 +0200

    Add set_flash method to controllers

--- spec/requests/home_controller_spec.rb
+++ spec/requests/home_controller_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Homes' do
+RSpec.describe HomeController do
+  it { expect(described_class.ancestors).to include(ApplicationController) }
+
   describe '.index' do
     it 'returns http success' do
       get root_path
