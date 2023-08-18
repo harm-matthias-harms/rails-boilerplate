271b091563cd4330469fc39dfe523d245c94f1fc
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-08-18 09:04:13 +0200

    Add current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,17 @@ class ApplicationController < ActionController::Base
   include Flashable
   include Pundit::Authorization
 
+  before_action :set_current_user, if: :user_signed_in?
   after_action :verify_authorized, except: :index
   after_action :verify_policy_scoped, only: :index
 
   protect_from_forgery with: :exception
 
   default_form_builder TailwindFormBuilder
+
+  private
+
+  def set_current_user
+    Current.user = current_user
+  end
 end
