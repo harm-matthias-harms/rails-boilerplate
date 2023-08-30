e45b4a06e8e8539b25f7e0e715f63be56bc46ec6
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-08-30 17:43:21 +0200

    Rework link component types

--- app/components/common/link_component.rb
+++ app/components/common/link_component.rb
@@ -1,12 +1,30 @@
 # frozen_string_literal: true
 
 class Common::LinkComponent < ApplicationComponent
-  attr_reader :type, :href, :data
+  attr_reader :link, :button, :href, :data
+
+  def type
+    return :link if link.present?
+
+    :button if button.present?
+  end
 
   def type_classes
     {
       link: 'link link-hover link-primary',
       button: 'btn'
-    }[type.to_sym]
+    }[type]
+  end
+
+  def button_class
+    return unless type == :button
+    return if button == true
+
+    case button.to_sym
+    when :primary
+      'btn-primary'
+    when :ghost
+      'btn-ghost'
+    end
   end
 end
--- spec/components/common/link_component_spec.rb
+++ spec/components/common/link_component_spec.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 RSpec.describe Common::LinkComponent, type: :component do
-  subject(:link) { described_class.new(type: :link, href: '#') }
+  subject(:link) { described_class.new(link: true, href: '#') }
 
-  let(:button) { described_class.new(type: :button, href: '#', classes: 'btn-primary', data: { turbo: false }) }
+  let(:button) { described_class.new(button: true, href: '#') }
+  let(:link_with_data) { described_class.new(link: true, href: '#', data: { test: 'test' }) }
 
   it 'renders a link' do
     render_inline(link) { 'Link' }
@@ -11,10 +12,16 @@ RSpec.describe Common::LinkComponent, type: :component do
     expect(page).to have_link('Link', href: '#')
   end
 
-  it 'renders additional classes' do
+  it 'renders button' do
     render_inline(button) { 'Button' }
 
-    expect(page).to have_selector('a.btn.btn-primary[data-turbo="false"]', text: 'Button')
+    expect(page).to have_selector('a.btn', text: 'Button')
+  end
+
+  it 'renders link with data' do
+    render_inline(link_with_data) { 'Link' }
+
+    expect(page).to have_selector('a[data-test="test"]')
   end
 
   describe '.type_classes' do
@@ -23,7 +30,20 @@ RSpec.describe Common::LinkComponent, type: :component do
       button: 'btn'
     }.each do |type, classes|
       it "returns #{classes} for type #{type}" do
-        expect(described_class.new(type:, href: '#').type_classes).to eq(classes)
+        expect(described_class.new(type => true, href: '#').type_classes).to eq(classes)
+      end
+    end
+  end
+
+  describe '.button_class' do
+    {
+      nil => nil,
+      true => nil,
+      primary: 'btn-primary',
+      ghost: 'btn-ghost'
+    }.each do |button, classes|
+      it "returns #{classes} for button #{button}" do
+        expect(described_class.new(button:, href: '#').button_class).to eq(classes)
       end
     end
   end
