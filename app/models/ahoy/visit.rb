3b08d4af483cdcda3f14cd0a31b37307ed238253
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-10-02 08:29:03 +0200

    Improve ahoy implicit colum order

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -2,6 +2,7 @@
 
 class Ahoy::Visit < ApplicationRecord
   self.table_name = 'ahoy_visits'
+  self.implicit_order_column = 'started_at'
 
   has_many :events, class_name: 'Ahoy::Event', dependent: :delete_all
   belongs_to :user, optional: true
