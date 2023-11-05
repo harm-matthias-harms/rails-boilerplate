4df8bc08f9c113acfe8e800fa9bb53977279251f
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-11-05 16:39:25 +0100

    Give hero heading default args

--- app/components/common/hero_component.rb
+++ app/components/common/hero_component.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Common::HeroComponent < ApplicationComponent
-  renders_one :heading, Common::HeadingComponent
+  renders_one :heading, -> { Common::HeadingComponent.new(tag: :h1) }
   renders_one :paragraph, Common::ParagraphComponent
   renders_one :image, Common::ImageComponent
   renders_many :actions, Common::LinkComponent
