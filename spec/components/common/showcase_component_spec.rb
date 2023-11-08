b9b22940d8230c23dacf6b712fe913cc2a9d730a
Harm Matthias Harms <matthias.harms@quis.de>
2023-11-08 23:07:48 +0100

    Add showcase spec

--- spec/components/common/showcase_component_spec.rb
+++ spec/components/common/showcase_component_spec.rb
@@ -1,13 +1,26 @@
 # frozen_string_literal: true
 
 RSpec.describe Common::ShowcaseComponent, type: :component do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:showcase_component) { described_class.new }
 
-  # it "renders something useful" do
-  #   expect(
-  #     render_inline(described_class.new(attr: "value")) { "Hello, components!" }.css("p").to_html
-  #   ).to include(
-  #     "Hello, components!"
-  #   )
-  # end
+  describe '.render' do
+    subject do
+      render_inline(showcase_component) do |showcase|
+        showcase.with_heading { 'Heading' }
+        showcase.with_description { 'Description' }
+        showcase.with_image(src: 'https://picsum.photos/200/300', alt: 'Image')
+        showcase.with_list do |list|
+          list.with_item { 'Item 1' }
+          list.with_item { 'Item 2' }
+          list.with_item { 'Item 3' }
+        end
+      end
+    end
+
+    it { is_expected.to have_css('.hero .hero-content') }
+    it { is_expected.to have_css('h2', text: 'Heading') }
+    it { is_expected.to have_css('p', text: 'Description') }
+    it { is_expected.to have_css('img[src="https://picsum.photos/200/300"][alt="Image"]') }
+    it { is_expected.to have_css('ul li', count: 3) }
+  end
 end
