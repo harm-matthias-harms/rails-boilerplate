b1f922ceeb2fa407a39a3f5471cb637f3492252d
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-12-03 14:16:06 +0100

    Add subscription policy

--- app/policies/pay/subscription_policy.rb
+++ app/policies/pay/subscription_policy.rb
@@ -2,10 +2,14 @@
 
 class Pay::SubscriptionPolicy < ApplicationPolicy
   def index?
-    user.present? && user.payment_processor.pay_customer.present?
+    user.present?
   end
 
   def new?
-    user.present? && user.payment_processor.subscription.blank?
+    user.present? && !premium?
+  end
+
+  def premium?
+    user&.payment_processor&.subscribed?(name: :premium)
   end
 end
--- spec/policies/pay/subscription_policy_spec.rb
+++ spec/policies/pay/subscription_policy_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
 RSpec.describe Pay::SubscriptionPolicy, type: :policy do
   subject(:policy) { described_class }
@@ -7,25 +7,29 @@ RSpec.describe Pay::SubscriptionPolicy, type: :policy do
 
   permissions :index? do
     it { is_expected.to permit(user, nil) }
-
-    context 'when user has no pay_customer' do
-      let(:user) { create(:user, :without_pay_customer) }
-
-      it { is_expected.not_to permit(user, nil) }
-    end
   end
 
   permissions :new? do
     it { is_expected.to permit(user, nil) }
 
     context 'when user has subscription' do
-      let(:user) { create(:user, :with_subscription) }
+      let(:user) { create(:user, :with_premium_subscription) }
 
       it { is_expected.not_to permit(user, nil) }
     end
   end
 
-  permissions :index?, :new? do
+  permissions :premium? do
+    it { is_expected.not_to permit(user, nil) }
+
+    context 'when user has premium subscription' do
+      let(:user) { create(:user, :with_premium_subscription) }
+
+      it { is_expected.to permit(user, nil) }
+    end
+  end
+
+  permissions :index?, :new?, :premium? do
     it { is_expected.not_to permit(nil, nil) }
   end
 end
