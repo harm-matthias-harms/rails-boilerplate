02a309b46427e0eb4201b03c241e39c1f71bff7e
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-12-03 20:15:00 +0100

    Add link to billing portal

--- spec/components/application_component_spec.rb
+++ spec/components/application_component_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe ApplicationComponent, type: :component do
   describe '.initialize' do
     it { expect(component.instance_variable_get(:@foo)).to eq('bar') }
   end
+
+  describe '.policy' do
+    subject { component.policy(record, policy_class:) }
+
+    let(:record) { double }
+    let(:policy_class) { nil }
+
+    before { allow(Pundit).to receive(:policy).and_return(:policy) }
+
+    it { is_expected.to eq(:policy) }
+
+    context 'when policy_class is not nil' do
+      let(:policy_class) { double }
+
+      before { allow(policy_class).to receive(:new).and_return(:policy_class) }
+
+      it { is_expected.to eq(:policy_class) }
+    end
+  end
 end
