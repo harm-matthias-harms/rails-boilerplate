c96cd50d5b0fc6a38b5854d17187b315417a8fdd
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-12-09 19:17:44 +0100

    Small UI improvement

--- app/components/common/showcase_component.rb
+++ app/components/common/showcase_component.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class Common::ShowcaseComponent < ApplicationComponent
+  attr_reader :rtl
+
   renders_one :heading, -> { Common::HeadingComponent.new(tag: :h2) }
   renders_one :description, 'Common::ParagraphComponent'
-  renders_one :image, 'Common::ImageComponent'
+  renders_one :image, ->(**kwargs) { Common::ImageComponent.new(shadow: true, **kwargs) }
   renders_one :list, -> { Common::ListComponent.new(icon: :checkmark) }
+
+  alias rtl? rtl
 end
