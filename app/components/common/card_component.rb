d6c0e9829ef7b700f7348d586f4cca7842c2c7af
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2023-12-10 17:50:46 +0100

    Add simple pricing section

--- app/components/common/card_component.rb
+++ app/components/common/card_component.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Common::CardComponent < ApplicationComponent
+  renders_one :heading, -> { Common::HeadingComponent.new(tag: :h3) }
+  renders_one :list, -> { Common::ListComponent.new(icon: :checkmark) }
+  renders_one :action, ->(**kwargs) { Common::LinkComponent.new(button: :primary, classes: 'btn-wide', **kwargs) }
 end
--- spec/components/common/card_component_spec.rb
+++ spec/components/common/card_component_spec.rb
@@ -4,9 +4,22 @@ RSpec.describe Common::CardComponent, type: :component do
   subject(:card_component) { described_class.new(classes: 'foo') }
 
   describe '.render' do
-    subject { render_inline(card_component) { 'Content' } }
+    subject do
+      render_inline(card_component) do |card|
+        card.with_heading { 'Heading' }
+        card.with_list do |list|
+          list.with_item { 'Item 1' }
+          list.with_item { 'Item 2' }
+        end
+        card.with_action { 'Action' }
+        'Content'
+      end
+    end
 
     it { is_expected.to have_css('.card.foo') }
     it { is_expected.to have_css('.card .card-body', text: 'Content') }
+    it { is_expected.to have_css('.card h3', text: 'Heading') }
+    it { is_expected.to have_css('.card li', count: 2) }
+    it { is_expected.to have_css('.card .card-actions .btn.btn-primary.btn-wide', text: 'Action') }
   end
 end
