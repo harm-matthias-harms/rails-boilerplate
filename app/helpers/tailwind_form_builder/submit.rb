a1cf43a099b7abf4916885e46f7297a9e6c9b6f2
Harm Matthias Harms <harmmatthias.harms@gmail.com>
2024-03-27 10:11:10 +0100

    Fix submit for none required fields

--- app/helpers/tailwind_form_builder/submit.rb
+++ app/helpers/tailwind_form_builder/submit.rb
@@ -7,14 +7,14 @@ module TailwindFormBuilder::Submit
     option[:class] = ['btn btn-primary btn-full']
 
     form_control do
-      required_submit_label + super(value, option)
+      [required_submit_label, super(value, option)].join.html_safe # rubocop:disable Rails/OutputSafety
     end
   end
 
   private
 
   def required_submit_label
-    return '' unless any_required?
+    return unless any_required?
 
     template.tag.label(class: 'label') do
       template.tag.span(class: 'text-xs text-neutral') do
