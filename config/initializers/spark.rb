7d31e68c6f35b6651ccf9e5cf037f132dc71600a
Harm Matthias Harms <matthias.harms@quis.de>
2025-01-07 17:39:42 +0100

    Fix hotwire spark config

--- config/initializers/spark.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.configure do |config|
-  break unless Rails.env.development?
-
-  config.hotwire.spark.html_paths += ['app/components']
-  config.hotwire.spark.stimulus_paths += ['app/components']
-  config.hotwire.spark.html_extensions += ['haml']
-end
